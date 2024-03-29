f96bc44cb76fa004476f973bb4f139363eee54db
EyaKadri <EyaKaad@gmail.com>
2024-03-29 23:01:34 +0100

    3-is_kind_of_class.py

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Contains the is_kind_of_class function
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Checks if the object is an instance
+    """
+    return isinstance(obj, a_class)
