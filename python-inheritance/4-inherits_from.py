92dc6f00fce7603c111ff3a549a5fb546cea7a68
EyaKadri <EyaKaad@gmail.com>
2024-03-29 23:10:00 +0100

    4-inherits_from.py

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,5 +5,7 @@ Contains the inherits_from function
 
 
 def inherits_from(obj, a_class):
-    """returns true if obj is  subclass of a_class, otherwise false"""
+    """
+    returns true if obj is  subclass of a_class, otherwise false
+    """
     return (issubclass(type(obj), a_class) and type(obj) != a_class)
