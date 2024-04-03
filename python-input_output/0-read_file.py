f2d423b20adad093b5ad0f4f5fe9ed35a8cb5fd5
EyaKadri <EyaKaad@gmail.com>
2024-04-03 20:18:29 +0200

    0-read_file.py

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""function containing the function read_file"""
+
+
+def read_file(filename=""):
+    """ reads a text file
+    """
+    try:
+        with open(filename, 'r', encoding='utf-8') as file:
+            for line in file:
+                print(line, end='')
+    except FileNotFoundError:
+        print(f"File '{filename}' not found.")
+
+read_file("example.txt")
