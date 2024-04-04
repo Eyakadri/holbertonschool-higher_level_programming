d559f6f10bdf3d86941a690d989de7d2d26fc290
EyaKadri <EyaKaad@gmail.com>
2024-04-04 22:57:40 +0200

    8-class_to_json.py

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """a function that returns the dictionary description"""
+
+
 def class_to_json(obj):
+    """Returns the dictionary description with simple data structure"""
     serializable_dict = {}
     for key, value in obj.__dict__.items():
         if isinstance(value, (list, dict, str, int, bool)):
