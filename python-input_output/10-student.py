a5e284971a3b7e5181074f5057b25cb66435415e
EyaKadri <EyaKaad@gmail.com>
2024-04-04 23:26:45 +0200

    10-student.py

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -7,10 +7,16 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-    
+
     def to_json(self, attrs=None):
-        """"""
         if attrs is None:
-            return self.__dict__
+            return {
+                key: getattr(self, key)
+                for key in sorted(self.__dict__.keys())
+            }
         else:
-            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+            return {
+                attr: getattr(self, attr)
+                for attr in attrs
+                if hasattr(self, attr)
+            }
