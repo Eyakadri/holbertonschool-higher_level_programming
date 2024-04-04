08d99c8c7bf8d8c60e0a95e265821750c75d9221
EyaKadri <EyaKaad@gmail.com>
2024-04-04 23:51:03 +0200

    12-pascal_triangle.py

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
-"""a function that returns a list of lists of integers"""
+"""function pascal_triangle"""
 
 
 def pascal_triangle(n):
+    """Returns a list of lists of integers"""
     if n <= 0:
         return []
+    if n == 1:
+        return [[1]]
 
-    triangle = [[1]]
-
-    for i in range(1, n):
-        row = [1]
-        for j in range(1, i):
-            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-        row.append(1)
-        triangle.append(row)
-
-    return triangle
+    pascal = [[1]]
+    for i in range(n - 1):
+        pascal.append([x + n for x, n in zip(pascal[-1] + [0],
+                                             [0] + pascal[-1])])
+    return (pascal)
