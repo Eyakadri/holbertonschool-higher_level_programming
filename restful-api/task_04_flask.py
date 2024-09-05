898006c4241f37db0f544a9d164fa5ac4f960f9a
EyaKadri <eyakaad@gmail.com>
2024-09-05 13:52:17 +0200

    task_04_flask.py

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,14 +1,8 @@
-from flask import Flask
-from flask import jsonify
-from flask import request
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-
-users = {
-    "jane": {"name": "Jane", "age": 28, "city": "Los Angeles"},
-    "john": {"name": "John", "age": 30, "city": "New York"}
-}
+users = {} 
 
 @app.route('/')
 def home():
@@ -16,12 +10,13 @@ def home():
 
 @app.route('/data')
 def get_users():
-    return jsonify(list(users.keys()))  # Return list of usernames
+    if not users:
+        return jsonify([])
+    return jsonify(list(users.keys()))
 
 @app.route("/status")
 def status():
     return "OK"
-
 
 @app.route("/users/<username>")
 def get_user(username):
@@ -29,11 +24,14 @@ def get_user(username):
     if user:
         return jsonify(user)
     else:
-        return jsonify({"error": "User not found"}), 404  # Return 404 if user not found
+        return jsonify({"error": "User not found"}), 404
 
 @app.route("/add_user", methods=["POST"])
 def add_user():
     user_data = request.json
+    if not user_data:
+        return jsonify({"error": "Invalid data"}), 400
+
     username = user_data.get("username")
     if not username or username in users:
         return jsonify({"error": "Invalid or duplicate username"}), 400
@@ -49,8 +47,6 @@ def add_user():
         "message": "User added",
         "user": users[username]
     })
-
-
 
 if __name__ == "__main__":
     app.run()
