6b17f8bfb2beb9217180e3fcb9a9c66b851d4392
ibrahim <ibrahimkammoun@outlook.com>
2024-11-24 21:27:03 +0100

    task_00_basic_serialization.py

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,31 +1,31 @@
-#!/usr/bin/env python3
-
 import json
 
 
 def serialize_and_save_to_file(data, filename):
     """
-    Serializes a Python dictionary
-    :param data: Dictionary to serialize
-    :param filename: Name of the file
+Serialize a Python dictionary to JSON and save it to a file
+    Args:
+        data (dict): The dictionary to serialize
+        filename (str): The output JSON file name
     """
     try:
-        with open(filename, 'w') as json_file:
-            json.dump(data, json_file)
+        with open(filename, 'w') as file:
+            json.dump(data, file)
     except Exception as e:
-        print(f"An error occurred while serializing and saving to file: {e}")
+        print(f"Error saving to file: {str(e)}")
 
 
 def load_and_deserialize(filename):
     """
-    Loads a JSON file and deserializes
-    :param filename: Name of the JSON file to read
-    :return: Dictionary deserialized
+    Load and deserialize data from a JSON file
+    Args:
+        filename (str): The input JSON file name
+    Returns:
+        dict: The deserialized dictionary
     """
     try:
-        with open(filename, 'r') as json_file:
-            data = json.load(json_file)
-        return data
+        with open(filename, 'r') as file:
+            return json.load(file)
     except Exception as e:
-        print(f"An error occurred while loading : {e}")
+        print(f"Error loading file: {str(e)}")
         return None
