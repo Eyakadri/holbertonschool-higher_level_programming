21f23f72751b9fe04000afac5f94d16dc767632c
ibrahim <ibrahimkammoun@outlook.com>
2024-11-27 10:28:06 +0100

    4-script.js

---
+++ javascript-dom_manipulation/4-script.js
@@ -0,0 +1,6 @@
+const addItem = document.querySelector('#add_item');
+addItem.addEventListener('click', function() {
+    const newItem = document.createElement('li');
+    newItem.textContent = 'Item';
+        document.querySelector('.my_list').appendChild(newItem);
+});
